0163df84731867322e18c15c206b1b2a1dc2cb57
Bedo321 <abdalrhman.ashry@gmail.com>
2024-11-27 22:23:08 +0200

    Log-in is working?

---
+++ Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Telecom_Company_Team_9.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+    </title>
+</head>
+<body>
+    <h1>
+    LOGIN PAGE
+    </h1>
+    <form id="form1" runat="server">
+        <div>
+            Mobile Num:<br />
+            <asp:TextBox ID="mobile" runat="server"></asp:TextBox> <br />
+            Password:<br />
+            <asp:TextBox ID="password" runat="server"></asp:TextBox> <br />
+            <asp:Button ID="login_button" runat="server" OnClick="login" Text="Log In" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecom_Company_Team_9
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void login(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+            String mob = mobile.Text;
+            String pass = password.Text;
+
+            SqlCommand login_func = new SqlCommand("AccountLoginValidation", conn);
+            login_func.CommandText = "SELECT dbo.AccountLoginValidation(@MobileNo, @password)";
+            login_func.CommandType = CommandType.Text;
+            login_func.Parameters.Add(new SqlParameter("@MobileNo", mob));
+            login_func.Parameters.Add(new SqlParameter("@password", pass));
+
+            conn.Open();
+            login_func.ExecuteNonQuery();
+            Boolean success = (bool)login_func.ExecuteScalar();
+            conn.Close();
+
+            if (success == true)
+            {
+                Response.Write("Hello");
+            }
+            else
+            {
+                Response.Write("manwab");
+
+            }
+        }
+    }
+}
