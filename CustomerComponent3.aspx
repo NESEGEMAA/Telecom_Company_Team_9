057d38f5fd8b522ea8b81e936c64b85e08961178
NESEGEMAA <nesegemaa@gmail.com>
2024-12-03 11:22:05 +0200

    Added a (maybe temporary) CustomerComponent3.aspx file, with the first requirement implemented (allShops View).

---
+++ CustomerComponent3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomerComponent3.aspx.cs" Inherits="Telecom_Company_Team_9.CustomerComponent3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>All shop details</p>
+
+        <asp:Button ID="ButtonAllShops" runat="server" Text="View all shops" OnClick="AllShopsView" />
+        <asp:GridView ID="AllShopsGridView" runat="server">
+        </asp:GridView>
+        <asp:Label ID="AllShopsErrorMessage" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ CustomerComponent3.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecom_Company_Team_9
+{
+    public partial class CustomerComponent3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AllShopsView(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            String data = "SELECT * FROM allShops";
+            SqlCommand cmd = new SqlCommand(data, conn);
+
+            SqlDataAdapter reader = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            reader.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                AllShopsGridView.DataSource = dt;
+                AllShopsGridView.DataBind();
+                AllShopsErrorMessage.Visible = false;
+            }
+            else
+            {
+                AllShopsErrorMessage.Text = "No benefits available to display.";
+                AllShopsErrorMessage.Visible = true;
+            }
+        }
+    }
+}
