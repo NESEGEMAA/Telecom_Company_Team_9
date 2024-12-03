6da518a474806607157d95c88bba6b85bfd54d14
NESEGEMAA <nesegemaa@gmail.com>
2024-12-03 19:20:22 +0200

    Added a view for the subscription plan renewal page.

---
+++ RenewSubscription.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RenewSubscription.aspx.cs" Inherits="Telecom_Company_Team_9.RenewSubscription" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Renew your subscription here:</h1>
+        <section>
+            <asp:Label ID="mobileNumberLabel" runat="server" Text="Input your Mobile Number"></asp:Label>
+        </section>
+        <section>
+            <asp:TextBox ID="InputMobileNumber" runat="server"></asp:TextBox>
+        </section>
+        <section>
+            <asp:Label ID="rechargeAmountLabel" runat="server" Text="Input the amount you want to recharge with"></asp:Label>
+        </section>
+        <section>
+            <asp:TextBox ID="InputAmount" runat="server"></asp:TextBox>
+        </section>
+            <asp:Label ID="paymentMethodLabel" runat="server" Text="Choose the payment method"></asp:Label>
+        <section>
+            <asp:DropDownList ID="paymentMethodDropDownList" runat="server">
+            <asp:ListItem>cash</asp:ListItem>
+            <asp:ListItem>credit</asp:ListItem>
+            </asp:DropDownList>
+        </section>
+        <section>
+            <asp:Label ID="planIDLabel" runat="server" Text="Input the plan ID you wanna renew"></asp:Label>
+        </section>
+        <section>
+            <asp:TextBox ID="InputPlanId" runat="server"></asp:TextBox>
+        </section>
+        <section>
+            <asp:Label ID="Message" runat="server" Text=""></asp:Label>
+        </section>
+        <asp:Button ID="renewButton" runat="server" Text="Renew" OnClick="Renew" />
+    </form>
+</body>
+</html>
---
+++ RenewSubscription.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecom_Company_Team_9
+{
+    public partial class RenewSubscription : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Renew(object sender, EventArgs e)
+        {
+            // Get the connection string from Web.config
+            string connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+
+            // SQL query to get data from the function
+            string data = "EXEC Initiate_plan_payment @mobile_num = @mobileNo, @amount = @inputAmount, @payment_method = @paymentMethod" +
+                ", @plan_id = @planID";
+
+            // Get the mobile number from the input field
+            Int32 mobileNumber = int.Parse(InputMobileNumber.Text);
+            Int32 rechargeAmount = int.Parse(InputAmount.Text);
+            string paymentMethod = paymentMethodDropDownList.SelectedValue;
+            Int32 planID = int.Parse(InputPlanId.Text);
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    using (SqlCommand cmd = new SqlCommand(data, conn))
+                    {
+                        // Add parameter to prevent SQL injection
+                        cmd.Parameters.AddWithValue("@mobileNo", mobileNumber);
+                        cmd.Parameters.AddWithValue("@inputAmount", rechargeAmount);
+                        cmd.Parameters.AddWithValue("@paymentMethod", paymentMethod);
+                        cmd.Parameters.AddWithValue("@planID", planID);
+
+                        // Open the connection
+                        conn.Open();
+
+                        // Execute the stored procedure
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            Message.Text = "Transaction completed";
+                        }
+                        else
+                        {
+                            Message.Text = "Transaction failed";
+                        }
+
+                        Message.Visible = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Message.Text = "An error occurred: " + ex.Message;
+                Message.Visible = true;
+            }
+        }
+    }
+}
