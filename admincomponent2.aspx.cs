4d595564fa0bbf917119789c0339326d5c70dc7c
Hisham Hani <hisham.hany26@gmail.com>
2024-12-04 17:47:38 +0200

    admin component 2

---
+++ admincomponent2.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecom_Company_Team_9
+{
+    public partial class admincomponent2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            Response.Write(Session["admin"]);
+        }
+    }
+}
