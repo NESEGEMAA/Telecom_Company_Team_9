eda46772fb5c38a8d2c10ea4155ad790a793e95b
NESEGEMAA <nesegemaa@gmail.com>
2024-12-05 21:49:20 +0200

    Forgot to commit this file in the Site.Master changed commit.

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace Telecom_Company_Team_9
     {
 
         /// <summary>
+        /// HeadContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+
+        /// <summary>
         /// navMenuPlaceholder control.
         /// </summary>
         /// <remarks>
