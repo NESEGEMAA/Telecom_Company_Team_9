d75b97b1420d9305d6764822cabfb99b7c4a3a30
Hisham Hani <hisham.hany26@gmail.com>
2024-12-06 01:49:02 +0200

    5th part

---
+++ AcceptedPaymentTransaction.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace Telecom_Company_Team_9
+{
+    public partial class AcceptedPaymentTransaction : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get the connection string from Web.config
+            string connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+
+            // SQL query to get data from the function
+            string data = "Exec Account_Payment_Points @mobile_num = @mobileNo";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    using (SqlCommand cmd = new SqlCommand(data, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@mobileNo", mobileNo);
+                        // Open the connection
+                        conn.Open();
+
+                        SqlDataAdapter reader = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        reader.Fill(dt);
+
+                        if (dt.Rows.Count > 0)
+                        {
+                            AcceptedPaymentTransactionView.DataSource = dt;
+                            AcceptedPaymentTransactionView.DataBind();
+                            Message.Visible = false;
+                        }
+                        else
+                        {
+                            Message.Text = "No Cashback available to display.";
+                            Message.Visible = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Message.Text = "An error occurred: " + ex.Message;
+                Message.Visible = true;
+            }
+        }
+    }
+}
